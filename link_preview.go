f7bc6349d48fa9df78ff6bcd7219580d1a10f1f1
Andrew Junzki <andrew@junzki.me>
2019-05-19 19:50:13 +0800

    Done link-preview functions.
    - Add tests.

--- link_preview.go
+++ link_preview.go
@@ -1,21 +1,23 @@
 package link_preview
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/PuerkitoBio/goquery"
-	"image"
+	"golang.org/x/net/html"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 type LinkPreview struct {
-	Title		string
-	Description	string
-	Image       *image.Image
-	ImageURL	string
-
-	Link		string
+	Title		string	`json:"title"`
+	Description	string	`json:"description"`
+	ImageURL	string	`json:"image"`
+	Link		string	`json:"website"`
 
 	parsed      *goquery.Document
+	client      *http.Request
 }
 
 type HTMLMetaAttr struct {
@@ -23,10 +25,16 @@ type HTMLMetaAttr struct {
 	Value   string
 }
 
-func PreviewLink(link string) (*LinkPreview, error) {
+func PreviewLink(link string, extraClient *http.Request) (*LinkPreview, error) {
 	preview := LinkPreview{}
 
 	preview.Link = link
+
+	if nil == extraClient {
+		preview.initClient()
+	} else {
+		preview.client = extraClient
+	}
 	err := preview.preview()
 
 	if err != nil {
@@ -36,8 +44,15 @@ func PreviewLink(link string) (*LinkPreview, error) {
 }
 
 
+func (p *LinkPreview) initClient() {
+	client, _ := http.NewRequest("GET", p.Link, nil)
+	p.client = client
+}
+
+
 func (p *LinkPreview) preview() error {
 	err := p.request()
+
 	if nil != err {
 		return err
 	}
@@ -48,7 +63,7 @@ func (p *LinkPreview) preview() error {
 
 
 func (p *LinkPreview) request() error {
-	res, err := http.Get(p.Link)
+	res, err := http.DefaultClient.Do(p.client)
 	if nil != err {
 		return err
 	}
@@ -72,11 +87,130 @@ func (p *LinkPreview) readTags() error {
 	}
 	p.Title = titleNode.Text()
 
-	metaNodes := p.parsed.Find("html > head > meta")
-	if 0 == metaNodes.Length() {
-		return errors.New("meta not found")
+	// Find `favicon.ico`.
+	linkNodes := p.parsed.Find("html > head > link")
+	for _, node := range linkNodes.Nodes {
+		for _, attr := range node.Attr {
+			switch strings.ToLower(attr.Key) {
+			case "rel":
+				if attr.Val != "icon" {
+					break
+				}
+				p.parseFavicon(node)
+			default:
+				continue
+			}
+		}
 	}
 
+	// Parse <meta> tags.
+	metaNodes := p.parsed.Find("html > head > meta")
+	for _, node := range metaNodes.Nodes {
+		for _, attr := range node.Attr {
+			switch strings.ToLower(attr.Key) {
+			case "property":
+				p.parseMetaProperties(attr.Val, node)
+				break
+			case "name":
+				if "description" == strings.ToLower(attr.Val) && "" == p.Description {
+					content := p.parseMetaContent(node)
+					p.Description = content
+					break
+				}
+			default:
+				continue
+			}
+		}
+	}
 
 	return nil
+}
+
+
+func (p *LinkPreview) parseMetaContent(node *html.Node) string {
+	var content string
+	for _, attr := range node.Attr {
+		switch strings.ToLower(attr.Key) {
+		case "content":
+			content = attr.Val
+			break
+		default:
+			continue
+		}
+	}
+
+	return content
+}
+
+
+func (p *LinkPreview) parseMetaProperties(nodeType string, node *html.Node) {
+	nodeType = strings.ToLower(nodeType)
+	if ! strings.HasPrefix(nodeType, "og:") {
+		return
+	}
+
+	slices := strings.Split(nodeType, ":")
+	if 2 != len(slices) {
+		return
+	}
+
+	nodeType = slices[1]
+	content := p.parseMetaContent(node)
+
+	switch nodeType {
+	case "description":
+		p.Description = content
+	case "image":
+		p.ImageURL = content
+	case "title":
+		p.Title = content
+	}
+}
+
+func (p *LinkPreview) parseFavicon(node *html.Node) {
+	var link string
+
+	for _, attr := range node.Attr {
+		switch strings.ToLower(attr.Key) {
+		case "href":
+			link = attr.Val
+			break
+		default:
+			continue
+		}
+	}
+
+	if "" == link {
+		return
+	}
+
+	if strings.HasPrefix("http://", link) || strings.HasPrefix("https://", link) {
+		p.ImageURL = link
+		return
+	}
+
+	parsedURL, _ := url.Parse(p.Link)
+	joinedURL := url.URL{
+		Scheme: parsedURL.Scheme,
+		Host:   parsedURL.Host,
+		Path:   link,
+	}
+
+	link = joinedURL.String()
+	if "" == p.ImageURL {
+		p.ImageURL = link
+	}
+}
+
+func (p *LinkPreview) ToJSON() string {
+	serialized, err := json.Marshal(p)
+	if err != nil {
+		return `{
+			"title": "",
+			"description": "",
+			"image": "",
+			"link": ""
+		}`
+	}
+	return string(serialized)
 }
--- preview_test.go
+++ preview_test.go
@@ -2,20 +2,61 @@ package link_preview
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 
-func TestPreviewLink(t *testing.T) {
-	var url = "https://github.com/aakash4525/py_link_preview/blob/master/link_preview/link_preview.py"
+func TestPreviewStandard(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		buf, err := ioutil.ReadFile("test_cases/case_standard.html")
+		if nil != err {
+			panic(err)
+		}
 
-	preview, err := PreviewLink(url)
-	if nil != err {
+		content := string(buf)
+		fmt.Fprint(w, content)
+	}))
+
+	link := "http://custom-domain.local/case.html"
+	client, _ := http.NewRequest("GET", server.URL, nil)
+
+ 	result, err := PreviewLink(link, client)
+ 	if err != nil {
+ 		t.Error(err)
+	}
+
+ 	assert.Equal(t, "test-title", result.Title)
+ 	assert.Equal(t, "test-desc", result.Description)
+ 	assert.Equal(t, "http://fake-image-url.image.png", result.ImageURL)
+ 	assert.Equal(t, link, result.Link)
+}
+
+
+func TestPreviewFallback(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		buf, err := ioutil.ReadFile("test_cases/case_fallback.html")
+		if nil != err {
+			panic(err)
+		}
+
+		content := string(buf)
+		fmt.Fprint(w, content)
+	}))
+
+	link := "http://custom-domain.local/case.html"
+	client, _ := http.NewRequest("GET", server.URL, nil)
+
+	result, err := PreviewLink(link, client)
+	if err != nil {
 		t.Error(err)
 	}
 
-	title := preview.Title
-	fmt.Println(title)
-
-
+	assert.Equal(t, "Title", result.Title)
+	assert.Equal(t, "meta-desc", result.Description)
+	assert.Equal(t, "http://custom-domain.local/favicon.ico", result.ImageURL)
+	assert.Equal(t, link, result.Link)
 }
