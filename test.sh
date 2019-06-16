ab9b860bea87fe0026e4125093a21984b5951a6d
Andrew Junzki <andrew@junzki.me>
2019-06-16 23:22:30 +0800

    Fix issues in coverage.

--- test.sh
+++ test.sh
@@ -3,8 +3,10 @@
 set -e
 echo "" > coverage.txt
 
+PACKAGE_NAME="github.com/Junzki/link-preview"
+
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic "$d"
+    go test -race -coverprofile=profile.out -covermode=atomic -coverpkg=${PACKAGE_NAME} "$d"
     if [[ -f profile.out ]]; then
         cat profile.out >> coverage.txt
         rm profile.out
